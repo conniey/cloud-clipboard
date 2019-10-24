751def3606c74dd4f44aafaec4d508ae87e2e610
Connie <conniey@microsoft.com>
2019-10-23 20:16:20 -0700

    Adding configuration files.

---
+++ src/main/java/com/conniey/cloudclipboard/models/AzureConfiguration.java
@@ -0,0 +1,38 @@
+package com.conniey.cloudclipboard.models;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:app.properties")
+@ConfigurationProperties(prefix = "aad")
+public class AzureConfiguration {
+    private String clientId;
+    private String clientSecret;
+    private String tenantId;
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+}
--- src/main/java/com/conniey/cloudclipboard/repository/AzureStorageRepository.java
+++ src/main/java/com/conniey/cloudclipboard/repository/AzureStorageRepository.java
@@ -1,6 +1,7 @@
 package com.conniey.cloudclipboard.repository;
 
 import com.conniey.cloudclipboard.models.Clip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,6 +10,9 @@ import reactor.core.publisher.Mono;
 @Repository
 @Profile("production")
 public class AzureStorageRepository implements ClipRepository {
+    @Autowired
+    public AzureStorageRepository()
+
     @Override
     public Flux<Clip> getClips() {
         return null;
