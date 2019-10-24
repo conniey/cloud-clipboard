71ba3c8c4d2d48f43d5d0d86ba581862a1ad3f6c
Connie <conniey@microsoft.com>
2019-10-24 14:36:11 -0700

    Fixing properties.

--- src/main/java/com/conniey/cloudclipboard/models/AzureCredentials.java
+++
@@ -1,4 +0,0 @@
-package com.conniey.cloudclipboard.models;
-
-public class AzureCredentials {
-}
---
+++ src/main/java/com/conniey/cloudclipboard/models/StorageConfiguration.java
@@ -0,0 +1,7 @@
+package com.conniey.cloudclipboard.models;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StorageConfiguration {
+}
